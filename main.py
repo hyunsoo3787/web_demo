cfa3f352eb57e8d539547fe0f2ecf874494f3f21
hyunsoo3787 <gustn3787@gmail.com>
2022-07-09 16:50:22 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from flask import Flask
+import logging
+import os
+import cv2
+from flask import Flask, render_template, request, jsonify
+import glob
 
+# create flask instance
 app = Flask(__name__)
 
+INDEX = os.path.join(os.path.dirname(__file__), 'index.csv')
 
+
+# main route
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+# search route
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == "POST":
+
+        try:
+            RES = []
+            RESULTS_ARRAY = glob.glob(".\\static\\res_*.png")
+            for temp in RESULTS_ARRAY:
+                RES.append(temp.split("\\")[-1])
+            # get url
+            image_url = request.form.get('img')
+            name = image_url.split("/")[-1]
+            temp = "res_" + image_url.split("/")[-1]
+            if temp in RES:
+                res = {"image":"../static/{}".format("res_"+name), "semantic":"../static/{}".format("sem_"+name)}
+                return jsonify(results=([res]))
+        except:
+            # return error
+            jsonify({"sorry": "Sorry, no results! Please try again."}), 500
 
 if __name__ == '__main__':
     app.run()
